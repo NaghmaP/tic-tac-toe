0e0fdc1411dba34f5401b97e98f1caaa92ff543f
NaghmaP <parween.naghma@gmail.com>
2020-02-06 17:23:34 +0530

    Create 2p-tictactoe.py

---
+++ 2p-tictactoe.py
@@ -0,0 +1,102 @@
+'''
+Two player TIC -TAC -TOE
+
+HOW TO PLAY: 
+Use numeric keypad to enter position of marker. 
+---------------                                                                                                                       
+TIC - TAC - TOE                                                                                                                       
+---------------                                                                                                                       
+  7 | 8 | 9                                                                                                                           
+  4 | 5 | 6                                                                                                                           
+  1 | 2 | 3
+
+'''
+def printBoard(board):
+    print("---------------")
+    print("  "+board[7]+'|'+board[8]+'|'+board[9])
+    print("  "+board[4]+'|'+board[5]+'|'+board[6])
+    print("  "+board[1]+'|'+board[2]+'|'+board[3])
+    print("---------------")
+def playerInput():
+    marker=''
+    while not (marker == 'X' or marker =='O'):
+        marker=input('Player 1: Do you want to be X or O? ').upper()
+    if marker=='X':
+        return ('X','O')
+    else:
+        return ('O','X')
+        
+
+def place_marker(board,marker,position):
+    board[position]=' '+marker+' '
+
+def space_check(board, position):
+    return board[position] == '   '
+    
+def player_choice(board):
+    while True:
+        pos=int(input('Enter position(1-9): '))
+        if pos in [1,2,3,4,5,6,7,8,9] and space_check(board,pos):
+            break;
+    return pos
+    
+def win_check(board,mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
+    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
+    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
+    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
+    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
+    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
+    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
+    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
+
+def board_full_check(board):
+    for i in range(1,10):
+        if space_check(board,i):
+            return False
+    return True
+
+#-----------------------------------------------------------------
+print("---------------")
+print("TIC - TAC - TOE")
+print("---------------")
+
+board=['   ']*10
+player1,player2=playerInput()
+
+win=False
+turn='P1'
+
+count=0
+while win==False:
+    if turn =='P1':
+        printBoard(board)
+        position=player_choice(board)
+        place_marker(board,player1,position)
+        count=+count
+        turn='P2'
+        if win_check(board,' '+player1+' '):
+            printBoard(board) 
+            print('Player1 won!')
+            win=True
+        else:
+            if board_full_check(board):
+                printBoard(board) 
+                print('Game is draw!')
+                win=True
+    else:
+        printBoard(board)
+        position=player_choice(board)
+        place_marker(board,player2,position)
+        count=+count
+        turn='P1'
+        if win_check(board,' '+player2+' '):
+            printBoard(board) 
+            print('Player2 won!')
+            win=True
+        else:
+            if board_full_check(board):
+                printBoard(board) 
+                print('Game is draw!')
+                win=True
+    
